a065783809c5e990215ca868e50010e8459a1ef5
Katie Kuwata <katie.kuwata@gmail.com>
2012-10-05 15:44:27 -0700

    Modified the code to use mouse movement other than gyro if not android. (Ease of testing on other platform)

--- Assets/GyroRotate.js
+++ Assets/GyroRotate.js
@@ -7,6 +7,7 @@ function Start () {
 }
 
 // Called every frame
+/*
 function Update () {
 	var gyro:Quaternion = Input.gyro.attitude;
 
@@ -14,4 +15,23 @@ function Update () {
 	gyro.y *= -1.0;
 
 	myTransform.localRotation = gyro;
+}
+*/
+function Update ()
+{
+     //#if UNITY_IPHONE  ||  UNITY_ANDROID
+     #if UNITY_ANDROID
+    
+     var gyro:Quaternion = Input.gyro.attitude;
+     gyro.x *= -1.0;
+     gyro.y *= -1.0;
+    
+     myTransform.localRotation = gyro;
+    
+     #else
+    
+     var mousePos:Vector2 = Vector2(Input.mousePosition.x/Screen.width-0.5,Input.mousePosition.y/Screen.height-0.5)*2;
+     myTransform.rotation = Quaternion.Euler(Vector3(mousePos.y,-mousePos.x,0)*180);
+    
+     #endif
 }
